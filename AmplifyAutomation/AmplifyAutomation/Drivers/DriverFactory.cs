38d403bcef314e97caaeaae7f79bf27efbe2df05
nealw <nealw@BILLS_LAPTOP>
2025-03-26 23:13:35 -0400

    This is the first committ, I have set up some architecture, as well as a base driver file

---
+++ AmplifyAutomation/AmplifyAutomation/Drivers/DriverFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace AmplifyAutomation.Drivers
+{
+
+    public class DriverFactory
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver GetDriver()
+        {
+            if (driver == null)
+            {
+                var options = new ChromeOptions();
+                driver = new ChromeDriver(options);
+                driver.Manage().Window.Maximize();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            }
+            return driver;
+        }
+
+        public static void QuitDriver()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver = null;
+            }
+        }
+    }
+}
